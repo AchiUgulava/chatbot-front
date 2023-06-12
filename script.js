73664f4843b0bda98a06eff7ddaecd3b1c919cdf
AchiUgulava <achiugulava@gmail.com>
2023-06-12 21:20:11 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var chatMessages = document.getElementById('chat-messages');
+    var userInput = document.getElementById('user-input');
+    var sendButton = document.getElementById('send-button');
+  
+    sendButton.addEventListener('click', function() {
+      var message = userInput.value;
+      if (message.trim() !== '') {
+        addMessage('user', message);
+        processUserMessage();
+        userInput.value = '';
+      }
+    });
+  
+    function addMessage(sender, message) {
+      var messageElement = document.createElement('div');
+      messageElement.classList.add('message');
+      messageElement.classList.add(sender);
+      messageElement.textContent = message;
+  
+      chatMessages.appendChild(messageElement);
+      chatMessages.scrollTop = chatMessages.scrollHeight;
+    }
+  
+    function processUserMessage(recievedMessage) {
+      // Process the user message and generate a response
+
+      //probly some api call to retrieve the message
+      //
+      //some recieved messa
+      var message = "lorem ipsum"
+      // Example response for testing purposes
+      var response = 'bot: ' + message;
+  
+      // Add a delay to simulate the response time
+      setTimeout(function() {
+        addMessage('chatbot', response);
+      }, 1000);
+    }
+  });
+  
