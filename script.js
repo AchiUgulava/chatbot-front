a17177a745f65ab753c1fac8190277e12bec6523
AchiUgulava <achiugulava@gmail.com>
2023-06-13 22:06:07 +0400

    fp1

--- script.js
+++ script.js
@@ -1,54 +1,68 @@
 // some ui stuff
 
-document.addEventListener('DOMContentLoaded', function() {
-  var dropdownToggle = document.querySelector('.dropdown-toggle');
-  var dropdownMenu = document.querySelector('.dropdown-menu');
+document.addEventListener("DOMContentLoaded", function () {
+  var dropdownToggle = document.querySelector(".dropdown-toggle");
+  var dropdownMenu = document.querySelector(".dropdown-menu");
 
-  dropdownMenu.addEventListener('click', function(event) {
+  dropdownMenu.addEventListener("click", function (event) {
     var selectedOption = event.target.textContent;
     dropdownToggle.textContent = selectedOption;
   });
 });
-
 //end of ui stuff
 
-document.addEventListener('DOMContentLoaded', function() {
-  var chatMessages = document.getElementById('chat-messages');
-  var userInput = document.getElementById('user-input');
-  var sendButton = document.getElementById('send-button');
+document.addEventListener("DOMContentLoaded", function () {
+  var chatMessages = document.getElementById("chat-messages");
+  var userInput = document.getElementById("user-input");
+  var sendButton = document.getElementById("send-button");
+  var sendIcon = document.querySelector(".send-icon");
 
-  sendButton.addEventListener('click', sendMessage);
-  userInput.addEventListener('keydown', function(event) {
-    if (event.key === 'Enter') {
+  sendButton.addEventListener("click", sendMessage);
+  userInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
       sendMessage();
     }
   });
 
   function sendMessage() {
     var message = userInput.value;
-    if (message.trim() !== '') {
-      addMessage('user', message);
+    var sendIcon = document.querySelector(".send-icon");
+    if (message.trim() !== "") {
+      addMessage("user", message);
       processUserMessage(message);
-      userInput.value = '';
+      userInput.value = "";
     }
+    sendIcon.classList.add("clicked");
+    setTimeout(function () {
+      sendIcon.classList.remove("clicked");
+    }, 100);
   }
 
   function addMessage(sender, message) {
-    var messageContainer = document.createElement('div');
-    messageContainer.classList.add('message-container');
+    var messageContainer = document.createElement("div");
+    messageContainer.classList.add("message-container");
 
-    var messageElement = document.createElement('div');
-    messageElement.classList.add('message');
+    var messageElement = document.createElement("div");
+    messageElement.classList.add("message");
     messageElement.classList.add(sender);
-    if(sender=="chatbot"){
+
+    if (sender === "chatbot") {
       var typewriter = new Typewriter(messageElement, {
-        delay: 50, // Delay between each character
+        delay: 30, // Delay between each character
         deleteSpeed: 20, // Speed of deleting characters
+        onComplete: function () {
+          // Remove cursor when typing is complete
+          var cursorElement = messageElement.querySelector(
+            ".Typewriter__cursor"
+          );
+          if (cursorElement) {
+            cursorElement.style.display = "none";
+          }
+        },
       });
-      
+
       typewriter.typeString(message).start();
-    }
-    else{
+    } else {
       messageElement.textContent = message;
     }
 
@@ -63,13 +77,13 @@ document.addEventListener('DOMContentLoaded', function() {
     // Probable API call to retrieve the message
     //
     // Some received message
-    var message = 'lorem ipsum';
+    var message = '"Lorem ipsum dolor sit amet, consectetur adipiscing elit."';
     // Example response for testing purposes
-    var response = 'bot: ' + message;
+    var response = "Bot: " + message;
 
     // Add a delay to simulate the response time
-    setTimeout(function() {
-      addMessage('chatbot', response);
+    setTimeout(function () {
+      addMessage("chatbot", response);
     }, 1000);
     // addMessage('chatbot', response);
   }
